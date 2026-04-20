cd2c7bfe6c0fd29c8aa704dff8bb4d3f97429673
Leanderson <161379464+Leoocx@users.noreply.github.com>
2026-04-19 23:33:06 -0300

    Atualizar o ClassificacaoService.java

--- service/ClassificacaoService.java
+++ service/ClassificacaoService.java
@@ -1,24 +1,34 @@
 package service;
 
-import java.util.ArrayList;
-
 import util.TextProcessor;
+import java.sql.SQLException;
+import java.util.List;
 
-/*
-	Responsabilidade:
-
-	Receber o email
-	Chamar o processamento
-	Chamar o NaiveBayesService
-	Retornar resultado final
-    
-    */
 public class ClassificacaoService {
+    private NaiveBayesService naiveBayesService;
+    private TreinamentoService treinamentoService;
+    private TextProcessor textProcessor;
     
-
-    public static boolean classificar(String email){
-        ArrayList<String> arr = TextProcessor.processarTexto(email);
-        return NaiveBayesService.isSpam(arr);
+    public ClassificacaoService(NaiveBayesService naiveBayesService, 
+                                TreinamentoService treinamentoService,
+                                TextProcessor textProcessor) {
+        this.naiveBayesService = naiveBayesService;
+        this.treinamentoService = treinamentoService;
+        this.textProcessor = textProcessor;
     }
-
+    
+    public boolean processarEmail(String email, boolean deveTreinar) throws SQLException {
+        // Pré-processamento
+        List<String> palavras = textProcessor.preprocessar(email);
+        
+        // Classificação
+        boolean isSpam = naiveBayesService.classificar(palavras);
+        
+        // Treinamento (se solicitado)
+        if (deveTreinar) {
+            treinamentoService.treinar(palavras, isSpam);
+        }
+        
+        return isSpam;
+    }
 }
