b94a7de667565a8f490d0240ab17d495247aed8d
Leanderson <161379464+Leoocx@users.noreply.github.com>
2026-05-22 20:05:10 -0300

    corrigindo problemas de dependências (faltou mvnw)

--- spam/src/main/java/service/NaiveBayesService.java
+++ spam/src/main/java/service/NaiveBayesService.java
@@ -14,6 +14,12 @@ public class NaiveBayesService {
 
     public static boolean isSpam(ArrayList<String> arr){
 
+        /*
+            if (probabilidade > *){ 
+                
+            }
+        
+        */
 
         return false;
     }
@@ -22,3 +28,21 @@ public class NaiveBayesService {
 
 
 }
+
+
+/*
+A probabilidade de um e-mail ser spam S, dado que uma determinada palavra W aparece, 
+é definida pelo lado esquerdo da equação acima, Pr(S|W) .
+
+O lado direito da equação apresenta a fórmula para calcular essa probabilidade. É ela:
+
+    a probabilidade de a palavra ocorrer no e-mail, dado que se trata de um e-mail de spam, 
+    Pr(W|S) multiplicada pela probabilidade de um e-mail ser spam, Pr(S) .
+    dividimos a probabilidade de a palavra ocorrer no e-mail, dado que se trata de um e-mail de spam, 
+    pelo resultado da multiplicação pela probabilidade de um e-mail ser spam.
+    mais a probabilidade de a palavra ocorrer no e-mail, dado que é um e-mail não spam Pr(W|¬S) 
+    multiplicada pela probabilidade de um e-mail não ser spam Pr(¬S) .
+
+
+
+*/
