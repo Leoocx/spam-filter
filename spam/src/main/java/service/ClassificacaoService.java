ac270757054e4a53db54e453c5ee170cdb4f1a3e
Leanderson <161379464+Leoocx@users.noreply.github.com>
2026-05-27 18:15:12 -0300

    update

--- spam/src/main/java/service/ClassificacaoService.java
+++ spam/src/main/java/service/ClassificacaoService.java
@@ -4,6 +4,11 @@ import util.TextProcessor;
 import java.sql.SQLException;
 import java.util.List;
 
+/*
+Essa classe recebe o email bruto e processa o texto usando a classe TextProcessor, em seguida aplica o algoritmo de Naive Bayes para saber se esse email é considerado spam ou não, em seguida determina se deve treinar ou não o modelo(por padrão, deveTreinar é true para ir treinando o modelo automaticamente, isso pode ser alterado na Main).
+*/
+
+
 public class ClassificacaoService {
     private NaiveBayesService naiveBayesService;
     private TreinamentoService treinamentoService;
